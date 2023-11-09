ffde4ca7dd4f4cd082b35c8dca1978efbe83724f
petsdom <ronalvrobin28@gmail.com>
2023-11-09 18:05:32 +0300

    g

---
+++ script.js
@@ -0,0 +1,30 @@
+let timer = document.getElementById("timer")
+
+var countDownDate = new Date("Dec 1, 2023 10:30:00").getTime();
+
+var myfunc = setInterval(function() {
+        // code goes here
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + "days "
+    document.getElementById("hours").innerHTML = hours + "hours " 
+    document.getElementById("mins").innerHTML = minutes + "minutes " 
+    document.getElementById("secs").innerHTML = seconds + "seconds " 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+    }
+}, 1000)
